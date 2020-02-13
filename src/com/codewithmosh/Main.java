9097e4ab18b6adfeac38288da78f2b09017002d8
Damilare Durojaye <oluwadamilare.durojaye@trivago.com>
2020-02-14 00:31:32 +0100

    used Hash map to find First Non Repeating Character

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        Map<Integer, String> map = new HashMap<>();
+        /*Map<Integer, String> map = new HashMap<>();
         map.put(1, "Mosh");
         map.put(2, "John");
         map.put(3, "Mary");
@@ -17,6 +17,32 @@ public class Main {
 
         for (var item : map.entrySet()) {
             System.out.println(item.getValue());
+        }*/
+
+        String words = "a green apple";
+        char lowest = findFirstNonRepeatingCharacter(words);
+
+        System.out.println(lowest);
+    }
+
+    private static char findFirstNonRepeatingCharacter(String words) {
+        var wordArr = words.toCharArray();
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char letter : wordArr) {
+            if (map.containsKey(letter)) {
+                map.put(letter, map.get(letter) + 1);
+                continue;
+            }
+            map.put(letter, 1);
         }
+        System.out.println(map);
+        for (char letter : wordArr) {
+            if (map.get(letter) == 1) {
+                return letter;
+            }
+        }
+        return Character.MIN_VALUE;
     }
 }
