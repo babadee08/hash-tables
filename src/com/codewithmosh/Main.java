c0ef498c40f30709bc0d62beb322015a735d1501
Damilare Durojaye <oluwadamilare.durojaye@trivago.com>
2020-02-14 00:07:26 +0100

    Internal implementation of the Hash tables using HashMap

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,8 +1,22 @@
 package com.codewithmosh;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Mosh");
+        map.put(2, "John");
+        map.put(3, "Mary");
+        System.out.println(map.get(2));
+        System.out.println(map.containsKey(2));
+        System.out.println(map.containsValue("Weed"));
+        System.out.println(map);
+
+        for (var item : map.entrySet()) {
+            System.out.println(item.getValue());
+        }
     }
 }
